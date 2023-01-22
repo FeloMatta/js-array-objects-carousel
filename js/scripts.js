6d5a56281f8c5e9c032375d81512b4190bb1e97a
FeloMatta <mattafelo@gmail.com>
2023-01-22 15:16:58 +0100

    completato esercizio

--- js/scripts.js
+++ js/scripts.js
@@ -21,3 +21,101 @@ const images = [
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
 ];
+
+const slidesContainer = document.querySelector('.slides');
+const thumbnailsContainer = document.querySelector('.thumbnails');
+
+images.forEach((singleImage) =>{
+
+    const newSlide = document.createElement('div');
+    newSlide.classList.add('slide');
+    newSlide.innerHTML = 
+    `
+                    <img src="${singleImage.image}" alt="${singleImage.title}">
+
+                    <div class="texts">
+                        <h3>
+                            ${singleImage.title}
+                        </h3>
+                        <p>
+                            ${singleImage.text}
+                        </p>
+                    </div>
+    `;
+
+    slidesContainer.append(newSlide);
+
+    const newThumbnail = document.createElement('div');
+    newThumbnail.classList.add('thumbnail');
+    newThumbnail.style.height =(100 / images.length) + '%';
+    newThumbnail.innerHTML = 
+    `
+                    <img src="${singleImage.image}" alt="${singleImage.title}">
+    `;
+
+    thumbnailsContainer.append(newThumbnail);
+
+});
+
+let currentSlide = 0;
+
+const allSlides = document.querySelectorAll('.slide');
+
+allSlides[currentSlide].classList.add('active');
+
+const allThumbnails = document.querySelectorAll('.thumbnail');
+allThumbnails[currentSlide].classList.add('active');
+
+const prevButton = document.querySelector('.prev');
+
+prevButton.addEventListener('click',
+
+    () => {
+
+        allSlides[currentSlide].classList.remove('active');
+        allThumbnails[currentSlide].classList.remove('active');
+
+        if(currentSlide > 0){    
+
+            currentSlide--;
+
+        }
+        else{
+
+            currentSlide = images.length - 1;
+
+        }
+
+        allSlides[currentSlide].classList.add('active');
+        allThumbnails[currentSlide].classList.add('active');
+
+    }
+
+);
+
+const nextButton = document.querySelector('.next');
+
+nextButton.addEventListener('click',
+
+    () => {
+
+        allSlides[currentSlide].classList.remove('active');
+        allThumbnails[currentSlide].classList.remove('active');
+
+        if(currentSlide < images.length - 1){
+
+            currentSlide++;
+
+        }
+        else{
+
+            currentSlide = 0;
+
+        }
+
+        allSlides[currentSlide].classList.add('active');
+        allThumbnails[currentSlide].classList.add('active');
+
+    }
+
+);
